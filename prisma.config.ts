9717f59cf7fcdec58f4348e5bc3f102c5b08c04f
Subham <subhamsangwan26@gmail.com>
2026-03-20 00:43:22 +0530

    Update prisma.config.ts for configuration adjustments

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,7 +3,6 @@
 // npm install --save-dev prisma dotenv
 import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
-
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
